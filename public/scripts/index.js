c63327b96f45d69b61e3cceee48e2e30ec675caf
Barrett Crusse <cvillebear@gmail.com>
2024-09-16 01:31:05 -0400

    Theme overhaul, add dynamic background

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,8 +6,14 @@ const subtitle = document.getElementById("subtitle");
 const socialButtons = document.getElementById("social-buttons");
 const learnMore = document.getElementById("learn-more");
 
+const BACKGROUND = document.getElementById('background-canvas');
+const BG = BACKGROUND.getContext('2d');
+
 const LIGHT_TEXT = '#e0e0e0';
 const DARK_TEXT = '#131313';
+
+let mouseX = 0;
+let mouseY = 0;
 
 let fadeQueue;
 
@@ -118,6 +124,161 @@ function createPortfolioItem(data) {
     /* ___________ */
 
     return container;
+}
+
+const gradients = document.querySelectorAll('.gradient.grow');
+const HOVER_ANIM_TIME = 16;
+const HOVER_SPREAD = 0.4;
+let hoverAnimTimer;
+
+let isGradientHovered = false;
+// for (let elem of gradients) {
+//     elem.addEventListener('pointerover', () => {
+//         elem.classList.add('hovered');
+//         isGradientHovered = true;
+//         hoverAnimTimer = HOVER_ANIM_TIME;
+//     });
+
+//     elem.addEventListener('pointerout', () => {
+//         elem.classList.remove('hovered');
+//         isGradientHovered = false;
+//     });
+// }
+
+
+let img = new Image();
+img.src = '/public/images/chain.png';
+
+let imgWidth = 30;
+let imgHeight = 30;
+let gap = 20;
+
+BACKGROUND.width = BACKGROUND.clientWidth;
+BACKGROUND.height = BACKGROUND.clientHeight;
+console.log(BACKGROUND.width, BACKGROUND.height)
+
+let offset = 0;
+let maxOffset = imgWidth + gap; //Math.sqrt((imgWidth * imgWidth) + (imgHeight * imgHeight));
+
+
+document.addEventListener('pointermove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+})
+
+const HOVER_EFFECT_DURATION = 14;
+const HOVER_EFFECT_STRENGTH = 1;
+let hoverEffectTimer = 0;
+let useHoverEffect = false;
+
+document.addEventListener('pointerover', (e) => handleHoverEffects(e));
+document.addEventListener('pointerdown', (e) => handleHoverEffects(e));
+document.addEventListener('pointerup', (e) => useHoverEffect = false);
+
+function handleHoverEffects(e) {
+    if(e.target.closest('a') !== null) {
+        useHoverEffect = true;
+    } else {
+        useHoverEffect = false;
+    }
+}
+
+addEventListener('resize', () => {
+    BACKGROUND.width = BACKGROUND.clientWidth;
+    BACKGROUND.height = BACKGROUND.clientHeight;
+    console.log(BACKGROUND.width, BACKGROUND.height)
+})
+
+updateBackground();
+
+function updateBackground() {
+    let canvasWidth = BACKGROUND.width;
+    let canvasHeight = BACKGROUND.height;
+    BG.font = "18px serif";
+
+    BG.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    offset = (offset + 0.05) % maxOffset;
+
+    // Cool 3D cloth-ish thing
+
+    // for(let i = 0; i < canvasWidth / gap; i++) {
+    //     for (let j = 0; j < canvasHeight / gap; j++) {
+    //         let x = i * gap;
+    //         let y = j * gap;
+
+    //         let dx = x - mouseX;
+    //         let dy = y - mouseY;
+
+    //         let div = 5;
+    //         let mult = 50;
+
+    //         let angle = Math.atan2(dx, dy);
+    //         let cx = Math.cos(angle);
+    //         let cy = Math.sin(angle);
+
+    //         let distance = Math.hypot(x - dx, y - dy);
+            
+    //         BG.fillStyle = 'black'
+    //         BG.fillText('•', x + (cx * distance), y + (cy * distance))
+    //     }
+    // }
+
+    for(let i = 0; i < canvasWidth / gap; i++) {
+        for (let j = 0; j < canvasHeight / gap; j++) {
+            let circleSize = 400;
+            let mult = 50;
+            let bulgeEffect = 0.24;
+            let fadeDist = 0.3;
+            let darknessBoost = 0.2;
+
+            let x = i * gap;
+            let y = j * gap;
+
+            let dx = (x - mouseX)// / div;
+            let dy = (y - mouseY)// / div;
+
+            let angle = Math.atan2(dx, dy);
+            let cx = Math.cos(angle);
+            let cy = Math.sin(angle);
+
+            let distance = Math.hypot(dx, dy) / circleSize;
+
+            bulgeEffect += bulgeEffect * scale(hoverEffectTimer, 0, HOVER_EFFECT_DURATION, 0, HOVER_EFFECT_STRENGTH) / 8;
+            fadeDist += fadeDist * scale(hoverEffectTimer, 0, HOVER_EFFECT_DURATION, 0, HOVER_EFFECT_STRENGTH) / 2;
+
+            let clampedDistance = Math.min(distance, bulgeEffect);
+            clampedDistance = bulgeEffect - clampedDistance;
+
+            let alpha = Math.max(scale(fadeDist - distance, 0, darknessBoost, 0, 1), 0.1);
+
+            let finalDistance = clampedDistance + (clampedDistance * scale(hoverEffectTimer, 0, HOVER_EFFECT_DURATION, 0, HOVER_EFFECT_STRENGTH));
+
+            let finalX = x + (dx * finalDistance);
+            let finalY = y + (dy * finalDistance);
+            
+            let currDistance = scale(HOVER_ANIM_TIME - hoverAnimTimer, 0, HOVER_ANIM_TIME, 0, bulgeEffect);
+            if (distance < currDistance) {
+                BG.fillStyle = `rgba(240, 107, 97, ${alpha})`;
+            } else {
+                BG.fillStyle = `rgba(255, 255, 255, ${alpha})`;
+            }
+
+            BG.fillText('•', finalX, finalY)
+        }
+    }
+    
+    if (useHoverEffect && hoverEffectTimer < HOVER_EFFECT_DURATION) {
+        hoverEffectTimer++;
+    } else if (!useHoverEffect && hoverEffectTimer > 0) {
+        hoverEffectTimer--;
+    }
+
+    requestAnimationFrame(updateBackground);
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 	/*
